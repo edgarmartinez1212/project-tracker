e2d3522ea05107ed6d2da73e63c87888d2987366
Edgar Martinez <edgarmartinez@Edgars-MBP.attlocal.net>
2023-05-31 20:47:28 -0500

    worked on modal and its functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,49 @@
-let currentDate = $("#current-date");
-let currentTime = $("#current-time");
+let currentDateEl = $("#current-date");
+let currentTimeEl = $("#current-time");
+let formSubmitEl = $("#form-submit");
+let projectNameEl = $("#project-name");
+let projectTypeEl = $("#project-type");
+let projectDateEl = $("#project-date");
+let projectDescEl = $("#project-description");
 
 function setTime() {
   let timerInterval = setInterval(function () {
     let today = dayjs();
-    currentDate.text(today.format("MMMM D, YYYY"));
-    currentTime.text(today.format("hh:mm:ss a"));
+    currentDateEl.text(today.format("MMMM D, YYYY"));
+    currentTimeEl.text(today.format("hh:mm:ss a"));
   }, 1000);
 }
 
+function clearModal(){
+    projectNameEl.text('');
+    projectNameEl.text('');
+    projectNameEl.text('');
+    projectNameEl.text('');
+}
+
+function handleFormSubmit(event) {
+  let newProject = {
+    name: projectNameEl.val(),
+    type: projectTypeEl.val(),
+    date: projectDateEl.val(),
+    description: projectDescEl.val(),
+  };
+
+  let projects = JSON.parse(localStorage.getItem("projects"));
+  projects.push(newProject);
+
+  localStorage.setItem("projects", JSON.stringify(projects));
+
+  clearModal();
+}
+
+function setUpLocalStorage() {
+  if (localStorage.getItem("projects") === null) {
+    localStorage.setItem("projects", JSON.stringify([]));
+  }
+}
+
 setTime();
+setUpLocalStorage();
+
+formSubmitEl.on("click", handleFormSubmit);
